32175136b3797d2aadff222e6bb65a72a2b3bd31
thaivhph28695 <thaivh@ancu.com>
2022-06-10 21:35:10 +0700

    push -2

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,4 @@
-const productList = [
-  {
-    id: 1,
-    name: "Big and Juicy Wagyu Beef Cheeseburger",
-    img: "https://res.cloudinary.com/do2d1jyoh/image/upload/v1654870907/lab3/1_t4clun.png",
-    time: "30 Minutes",
-    type: "Snack",
-  },
-  {
-    id: 2,
-    name: "Fresh Lime Roasted Salmon",
-    img: "https://res.cloudinary.com/do2d1jyoh/image/upload/v1654870905/lab3/2_vsihxs.png",
-    time: "30 Minutes",
-    type: "Fish",
-  },
-  {
-    id: 3,
-    name: "The Best Easy One Pot Chicken and Rice",
-    img: "https://res.cloudinary.com/do2d1jyoh/image/upload/v1654870907/lab3/3_qcpeft.png",
-    time: "30 Minutes",
-    type: "Snack",
-  },
-  {
-    id: 4,
-    name: "Fresh and Healthy Mixed Mayonnaise",
-    img: "https://res.cloudinary.com/do2d1jyoh/image/upload/v1654870908/lab3/4_kj8sa2.png",
-    time: "30 Minutes",
-    type: "Healthy",
-  },
-  {
-    id: 5,
-    name: "The Creamiest Creamy Chicken",
-    img: "https://res.cloudinary.com/do2d1jyoh/image/upload/v1654870910/lab3/5_ozwsuf.png",
-    time: "30 Minutes",
-    type: "Noodles",
-  },
-  {
-    id: 6,
-    name: "Fruity Pancake with Orange & Blueberry",
-    img: "https://res.cloudinary.com/do2d1jyoh/image/upload/v1654870909/lab3/6_orx15f.png",
-    time: "30 Minutes",
-    type: "Sweet",
-  },
-];
+import { productList } from "./data";
 
 function showProducts(products) {
   if (!Array.isArray(products) || products.length == 0) return false;
